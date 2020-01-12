6f362052749cd60e3b1ebacf62492fbdcf5604d4
jiangyu <jiangyuwise@gmail.com>
2020-01-12 12:35:55 +0800

    第 3 题

---
+++ src/main/java/q3/Solution.java
@@ -0,0 +1,34 @@
+package q3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 求不含重复字符的最长子串的长度
+ * @author admin
+ * @date 2020/1/12 11:44
+ */
+public class Solution {
+
+    public int length(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int length = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                if (j - i > length) {
+                    length = j - i;
+                }
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.length(" "));
+    }
+}
